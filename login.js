f2c247ea517a900b2d3c22ef857042b590120987
PSC-Eclairage <139701706+PSC-Eclairage@users.noreply.github.com>
2023-07-16 17:53:04 +0200

    Update login.js

--- login.js
+++ login.js
@@ -11,7 +11,7 @@ function connexion()
   loginEmailPassword(username,password).then(user => {
     if(user != null)
     {
-    document.location.href="/index.html";
+    document.location.href="/psc-eclairage/index.html";
     }
     else 
     {
